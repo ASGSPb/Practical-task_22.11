ee2d92f760815d76ed26b22f532b9ef5201171b5
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-24 01:04:17 +0300

    practical task

---
+++ Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+
+// Console.WriteLine("Введите число не более 5 знаков");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+int num = new Random().Next(10000, 100000);
+int modul = Math.Abs ( num );
+string array = Convert.ToString ( modul );
+
+if ( array[0] == array[4] && array[1] == array[3] ) Console.WriteLine("Число "+ num + " число палиндром");
+else Console.WriteLine("Число "+ num + " палиндромом НЕ является");
---
+++ Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.WriteLine("Введите число N");
+double N = Convert.ToDouble(Console.ReadLine());
+double first = 1;
+
+while (  first <= N )
+{
+    Console.Write($""+ Math.Pow(first, 3) + ", ");
+    first++;
+}
+
+
